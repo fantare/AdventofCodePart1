db0232a70610b9c07195b213d07ef942103945c0
RoseF1 <1767240-RoseF1@users.noreply.replit.com>
2023-12-01 23:59:38 +0000

    Initial commit

---
+++ .ccls-cache/@home@runner@AdventofCodePart1/main.cpp
@@ -0,0 +1,50 @@
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <ctype.h>
+#include <vector>
+
+using namespace std;
+
+ifstream inFile;
+string line;
+
+int main()
+{
+
+
+    int firstNum = -1, sum = 0;
+    string num1, num2;
+    bool nextNum = true;
+
+    inFile.open("aocIn.txt");
+    if (inFile.is_open()) {
+        while (getline(inFile, line)) {
+            for (int i = 0; i < line.length(); i++) {
+                if (isdigit(line[i]) && firstNum == -1) {
+                    num1 = line[i];
+                    num2 = num1;
+                    firstNum = i;
+                }
+                else if (isdigit(line[i])) {
+                    num2 = line[i];
+
+                }
+            }
+            firstNum = -1;
+
+            if (!num1.empty() && num2.empty()) {
+                sum += stoi(num2 + num2);
+            }
+            else if (!num1.empty() && !num2.empty()) {
+                sum += stoi(num1 + num2);
+            }
+
+        }
+    }
+
+    std::cout << sum << endl;
+
+    inFile.close();
+    return 0;
+}
